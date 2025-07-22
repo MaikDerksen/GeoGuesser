45406eee32c1af1a36081a1b94fd709326cbcd7b
Maik Derksen <maikderksen2005@gmail.com>
2025-07-22 18:13:59 +0000

    use .env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDLm1o7egrh_Z8J_rXiLyOz_OBIIG-6OI4",
-  authDomain: "geocompass-l29mm.firebaseapp.com",
-  projectId: "geocompass-l29mm",
-  storageBucket: "geocompass-l29mm.appspot.com",
-  messagingSenderId: "135978667752",
-  appId: "1:135978667752:web:7c7b74d3e4f6a1d769835d"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
